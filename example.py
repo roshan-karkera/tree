cca488a08151f28f410db9c125c1f232a0b1f735
roshan-karkera <roshan.karkera12345@gmail.com>
2020-09-10 23:40:44 +0530

    first commit

---
+++ example.py
@@ -0,0 +1,5 @@
+import shelve
+
+for obj in  dir(shelve.Shelf):
+    if obj[0] !='_':
+        print(obj)
---
+++ turtledemo.py
@@ -0,0 +1,11 @@
+done = "Well done,you have finished your drawing"
+
+from turtle import *
+
+forward(150)
+right(250)
+circle(150)
+forward(150)
+
+done()
+print(done)
